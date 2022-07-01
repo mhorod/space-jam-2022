19fca707848a6d491643b19297b0687167404f76
Michal Horodecki <michalhorodecki2002@gmail.com>
2022-07-01 23:32:49 +0200

    organized assets

---
+++ locations.py
@@ -0,0 +1,13 @@
+import pygame as pg
+
+from level import Level
+
+
+class Location(Level):
+    def __init__(self, *args, **kwargs):
+        pass
+
+
+class Garden(Location):
+    def __init__(self, *args, **kwargs):
+        pass
